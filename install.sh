1068df4ac2fcdb8c2fd656cb2b3973e17cd21ff2
BloodyChaton <49313671+BloodyChaton@users.noreply.github.com>
2019-05-14 19:47:54 +0200

    Remove older version

--- install.sh
+++
@@ -1,7 +0,0 @@
-cd ~/
-wget https://download.java.net/java/GA/jdk12/GPL/openjdk-12_linux-x64_bin.tar.gz
-tar xzvf openjdk-12_linux-x64_bin.tar.gz
-me = $(whoami)
-su - root -c "mv /home/$me/jdk-12 /opt/jdk"
-su - root -c "touch /etc/profile.d/jdk.sh"
-
