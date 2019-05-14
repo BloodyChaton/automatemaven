c5a85d1b6097a509f1e41e226126bdaaa0bc6c1a
BloodyChaton <49313671+BloodyChaton@users.noreply.github.com>
2019-05-14 19:52:13 +0200

    Installation of Jenkins

---
+++ install-jenkins.sh
@@ -0,0 +1,3 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install epel-release jenkins
