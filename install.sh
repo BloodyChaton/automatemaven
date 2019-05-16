c778590377cd144f7fd7f94d76f8dcebab370784
BloodyChaton <49313671+BloodyChaton@users.noreply.github.com>
2019-05-16 17:30:09 +0200

    Update

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ touch /etc/profile.d/jdk.sh
 JAVA_HOME=/opt/java >> /etc/profile.d/jdk.sh
 PATH=$PATH:$JAVA_HOME/bin >> /etc/profile.d/jdk.sh
 export PATH JAVA_HOME >> /etc/profile.d/jdk.sh
-wget https://www-eu.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gztar xzvf apache-maven-3.6.0-bin.tar.gz
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+tar xzvf apache-maven-3.6.1-bin.tar.gz
 mv apache-maven-3.6.1 /opt/maven-3.6.1
 touch /etc/profile.d/maven.sh
 MAVEN_HOME=/opt/maven-3.6.1 >> /etc/profile.d/maven.sh
